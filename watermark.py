f7768ba91b7d462af0b35bbda0fc578e0f9d30ac
MolassesLover <60114762+MolassesLover@users.noreply.github.com>
2023-05-16 23:59:56 +0200

    Logging improvements

--- watermark.py
+++ watermark.py
@@ -1,23 +1,29 @@
 #!/usr/bin/env python
-
 import argparse
 import cv2
 import os
+import sys
 from imwatermark import WatermarkEncoder, WatermarkDecoder
+from colorama import Fore
+
 
 # This variable determines what AI to watermark for
 watermark_type = "SDV2"  # In this case, Stable Diffusion v2.0
 
 def decode(file, decoder):
-    print(f":: Decoding {file}")
+    print(f":: {Fore.BLUE}Decoding {Fore.MAGENTA}{file}{Fore.RESET}")
 
     image_file = cv2.imread(file)
     watermark = decoder.decode(image_file, "dwtDct")
     mark = watermark.decode("utf-8")
-    print(f":: {file} - watermark: {mark}, {watermark}")
+    # Check if there is a watermark
+    if watermark != b'\x00\x00\x00\x00':
+        print(f":: {Fore.MAGENTA}{file}{Fore.RESET} - Watermark: {mark}, {watermark.decode()}")
+    elif watermark == b'\x00\x00\x00\x00':
+        print(f":: {Fore.YELLOW}Warning{Fore.RESET}: No watermark found for {Fore.MAGENTA}{file}{Fore.RESET}!")
 
 def encode(file, encoder):
-    print(f":: Encoding {file}")
+    print(f":: {Fore.BLUE}Encoding {Fore.MAGENTA}{file}{Fore.RESET}")
 
     encoder.set_watermark("bytes", watermark_type.encode("utf-8"))
 
@@ -34,6 +40,11 @@ if __name__ == "__main__":
     argumentParser.add_argument("--decode", action="store_true")
     argumentParser.add_argument("--directory", type=str)
     arguments = argumentParser.parse_args()
+
+    if not len(sys.argv) > 1:
+        print(f":: {Fore.RED}Error{Fore.RESET}: No arguments have been provided!")
+        argumentParser.print_help()
+        sys.exit(1)
 
     amount_encoded: int = 0
     amount_decoded: int = 0
@@ -58,14 +69,14 @@ if __name__ == "__main__":
     # To do: Reduce code duplication
 
     # Print without a newline
-    print(f":: Encoded {amount_encoded} image", end='')
+    print(f":: {Fore.BLUE}Encoded {Fore.MAGENTA}{amount_encoded}{Fore.RESET} image", end='')
     if amount_decoded != 1:
         print("s") # Add an 's' to the word 'image'
     elif amount_decoded == 1:
         print() # Only print a newline character
     
     # Print without a newline
-    print(f":: Decoded {amount_decoded} image", end='')
+    print(f":: {Fore.BLUE}Decoded {Fore.MAGENTA}{amount_decoded}{Fore.RESET} image", end='')
     if amount_decoded != 1:
         print("s") # Add an 's' to the word 'image'
     elif amount_decoded == 1:
